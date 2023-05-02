f91d6dda97150256a1e7affea940c36f32af14bd
unknown <manuel.peli9@gmail.com>
2023-05-02 17:58:00 +0200

    add inventory background

--- Assets/ARDKExamples/RemoteAuthoring/Inventory/EnableDisableMenu.cs
+++ Assets/ARDKExamples/RemoteAuthoring/Inventory/EnableDisableMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EnableDisableMenu : MonoBehaviour
 {
     public GameObject UI_Inventory;
+    public GameObject panel;
+    public GameObject footer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +22,17 @@ public class EnableDisableMenu : MonoBehaviour
     public void WhenButtonClicked()
     {
         if (UI_Inventory.activeInHierarchy) 
+        {
+            panel.SetActive(true);
+            footer.SetActive(true);
             UI_Inventory.SetActive(false);
+        }
+            
         else 
+        {
+            panel.SetActive(false);
+            footer.SetActive(false);
             UI_Inventory.SetActive(true);
-
+        }
     }
 }
